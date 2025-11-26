d71f0fc308885eea45288e3bfe603e82ba6841ed
student <student@ARM-3041>
2025-11-26 18:58:09 +0200

    0.1

--- p311_music_store/MainWindow.xaml.cs
+++ p311_music_store/MainWindow.xaml.cs
@@ -25,7 +25,12 @@ namespace p311_music_store
             InitializeComponent();
         }
 
-        void add_new_data(object sender,  RoutedEventArgs e) { }
+        void add_new_data(object sender,  RoutedEventArgs e) 
+        {
+            AddNewDataWindow add_new_data_window = new AddNewDataWindow();
+            add_new_data_window.Show();
+        
+        }
         void show_new_records(object sender, RoutedEventArgs e) { }
         void show_most_popular_records(object sender, RoutedEventArgs e) { }
         void show_most_popular_groups(object sender, RoutedEventArgs e) { }
